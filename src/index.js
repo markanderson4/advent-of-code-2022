a05f8b97ddfa2176cbfaec5d773e48cddc90a808
Mark Anderson <markanderson@WTMBP-Mark-Anderson.local>
2022-12-01 14:36:47 -0500

    day 1 solutions

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+    return arr;
+}
+
+var allData = syncReadFile('data/day1Input.txt');
+
+var elfNumber = 0;
+var elfCalories = [0];
+
+for(var i = 0; i < allData.length; i++){
+    if (allData[i].length != 0){
+        elfCalories[elfNumber] += parseInt(allData[i]);
+    }
+    else {
+        elfNumber++;
+        elfCalories.push(0)
+    }
+}
+
+var numArray = [140000, 104, 99];
+numArray.sort(function(a, b) {
+  return a - b;
+});
+
+console.log("Highest elf calories: " + Math.max.apply(Math, elfCalories));
+
+elfCalories.sort(function(a, b) { return a - b; });
+
+console.log("Sum of top 3 calorie counts: " + (elfCalories[elfCalories.length - 1] + elfCalories[elfCalories.length - 2] + elfCalories[elfCalories.length - 3]) )
+
