8a3f3f772a9c3eb9a23d3ff8f4d07f0bf3b5069d
Mark Anderson <markanderson@WTMBP-Mark-Anderson.local>
2022-12-06 13:55:07 -0500

    cleanup on day 5 code

--- src/day5.js
+++ src/day5.js
@@ -7,59 +7,67 @@ function syncReadFile(filename) {
 }
 
 var allData = syncReadFile('data/day5input.txt');
-var i = 0;
-while(allData[i].length != 0){
-    i++;
-}
 
-var inputData = allData.slice(0, i)
-var commandData = allData.slice(i+1, allData.length)
-
-var labelRow = inputData[i-1];
-var bucketLocations = new Array()
-for(var k = 0; k < labelRow.length; k++) {
-    if(labelRow[k] != ' '){
-        bucketLocations.push(k)
+function getDividingLineIndex(allData) {
+    var i = 0;
+    while(allData[i].length != 0){
+        i++;
     }
+    return i;
 }
-//console.log("bucketLocations: " + bucketLocations)
 
-var allBuckets = [];
-for(var stackNumber = 0; stackNumber < bucketLocations.length; stackNumber++) {
-    var currentStack = new Array();
-    for(var j = i-2; j >= 0; j--) {
-        var currentRow = inputData[j]
-        //console.log("currentRow: " + currentRow + "\ncurentRow[stackNumber]: " + currentRow[bucketLocations[stackNumber]])
-        if(currentRow[bucketLocations[stackNumber]] != ' '){
-            //console.log("added")
-            currentStack.push(currentRow[bucketLocations[stackNumber]])
+function getBucketColumnLocations(labelRow) {
+    var bucketLocations = new Array()
+    for(var k = 0; k < labelRow.length; k++) {
+        if(labelRow[k] != ' '){
+            bucketLocations.push(k)
         }
     }
-    //console.log("currentStack (in loop): " + currentStack);
-    allBuckets.push(currentStack);
+    return bucketLocations;
 }
 
-//console.log(allBuckets)
-// copy by value! Not by reference!
-var allBucketsPartTwo = allBuckets;
+function getAllBuckets2dArray(bucketLocations, startingBucketIndex, inputData) {
+    var allBuckets = [];
+    for(var stackNumber = 0; stackNumber < bucketLocations.length; stackNumber++) {
+        var currentStack = new Array();
+        for(var i = startingBucketIndex; i >= 0; i--) {
+            var currentRow = inputData[i]
+            if(currentRow[bucketLocations[stackNumber]] != ' '){
+                currentStack.push(currentRow[bucketLocations[stackNumber]])
+            }
+        }
+        allBuckets.push(currentStack);
+    }
+    return allBuckets
+}
+
+const dividingLineIndex = getDividingLineIndex(allData)
+const inputData = allData.slice(0, dividingLineIndex)
+const commandData = allData.slice(dividingLineIndex+1, allData.length)
+const labelRow = inputData[dividingLineIndex-1];
+var startingBucketIndex = dividingLineIndex-2;
+const bucketLocations = getBucketColumnLocations(labelRow)
+
+var allBuckets = getAllBuckets2dArray(bucketLocations, startingBucketIndex, inputData)
+var allBucketsPartTwo = getAllBuckets2dArray(bucketLocations, startingBucketIndex, inputData);
 
 for(var commandIterator = 0; commandIterator < commandData.length; commandIterator++) {
     const command = commandData[commandIterator].split(" ")
     const oldStackIndex = parseInt(command[3])-1
     const newStackIndex = parseInt(command[5])-1
     const payloadSize = parseInt(command[1])
-    // for(var loop = 0; loop < payloadSize; loop++){
-    //     const payload = allBuckets[oldStackIndex].pop()
-    //     allBuckets[newStackIndex].push(payload)
-    // }
+    
+    // Part One
+    for(var loop = 0; loop < payloadSize; loop++){
+        const payload = allBuckets[oldStackIndex].pop()
+        allBuckets[newStackIndex].push(payload)
+    }
+
+    // Part Two
     const payload2 = allBucketsPartTwo[oldStackIndex].splice(allBucketsPartTwo[oldStackIndex].length-payloadSize, payloadSize)
-    console.log(payload2)
     for(var loopdeloop = 0; loopdeloop < payload2.length; loopdeloop++){
-        console.log(payload2[loopdeloop])
         allBucketsPartTwo[newStackIndex].push(payload2[loopdeloop])
     }
-    console.log("new receiving stack: " + allBucketsPartTwo[newStackIndex])
-    //console.log("old stack: " + allBuckets[oldStackIndex] + "\n newStack: " + allBuckets[newStackIndex] + "\n")
 }
 
 var topValuesPartOne = "";
@@ -69,11 +77,7 @@ for(var looper = 0; looper < bucketLocations.length; looper++){
     var currentBucketPartTwo = allBucketsPartTwo[looper]
     topValuesPartOne += currentBucketPartOne[currentBucketPartOne.length-1]
     topValuesPartTwo += currentBucketPartTwo[currentBucketPartTwo.length-1]
-    console.log()
 }
 
 console.log("Part One: " + topValuesPartOne)
 console.log("Part Two: " + topValuesPartTwo)
-
-
-
